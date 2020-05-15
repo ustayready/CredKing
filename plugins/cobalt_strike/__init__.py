a11687b12eb31e1f27c828c95b179c38e05d8d24
Jonathan Echavarria <jrec93+github@gmail.com>
2020-05-15 18:41:20 -0400

    Added cobalt strike module

---
+++ plugins/cobalt_strike/__init__.py
@@ -0,0 +1,11 @@
+def validate(args):
+    if 'host' in args.keys():
+        return True,None
+    else:
+        error = "Missing port argument, specify as --host 1.2.3.4"
+        return False,error
+    if 'port' in args.keys():
+        return True,None
+    else:
+        error = "Missing port argument, specify as --port 50050"
+        return False, error
