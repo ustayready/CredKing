042e226a4aa8c110b9e0503deeb0103b698372fa
hawkeyeone <ron@Desktop-Ron.localdomain>
2020-07-03 22:20:39 -0400

    Adding TODO items

---
+++ credkingGCP.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+
+#TODO Get Project
+
+#TODO: Get Locations
+
+#TODO: Upload Code
+
+#TODO: Create Function 
+
+#TODO: Modify Pluging to work in GCP and AWS
+
+#TODO: Thread the above code
+
+#TODO: Delete Function
+
+#TODO: Delete Code
+
