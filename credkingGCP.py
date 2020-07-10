1d4bcefb8eabc86075dad5a50e45bf9bca670a2d
hawkeyeone <ron@Desktop-Ron.localdomain>
2020-07-10 11:21:43 -0400

    Param upto creating a function

--- credkingGCP.py
+++ credkingGCP.py
@@ -22,26 +22,20 @@ storage_service = build('storage','v1',credentials=credentials)
 #TODO Get Project
 project = 'canvas-network-282101'
 
-
-#print(storage_service.buckets().list(project=project).execute())
-
-# Get Locations
-locations = service.projects().locations().list(name=f'projects/{project}').execute()
-print(len(locations['locations']))
-#print(json.dumps(locations,indent=2))
-print(locations['locations'][0])
+def log_entry(entry):
+    ts = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'[{ts}] {entry}')
 
 #print(service.projects().locations().functions().list(parent=locations['locations'][0]['name']).execute())
 
 
 # Uploading Code
-'''This needs to be done via the rest API via signed url - need to read up on what that is'''
 # Creating a bucket
 project = 'canvas-network-282101'
 
 # Creating a bucket
 body = {'name':'credkinggcp123456'}
-print(storage_service.buckets().insert(project=project,predefinedAcl="projectPrivate",body=body).execute())
+log_entry(storage_service.buckets().insert(project=project,predefinedAcl="projectPrivate",body=body).execute())
 
 # Uploading a file from a created bucket
 storage_client = storage.Client(project=project,credentials=credentials)
@@ -59,19 +53,34 @@ def create_zip(plugin):
     build_zip = 'build/{}_{}.zip'.format(plugin, random_name)
 
     with lock:
-        print('Creating build deployment for plugin: {}'.format(plugin))
+        log_entry('Creating build deployment for plugin: {}'.format(plugin))
         shutil.make_archive(build_zip[0:-4], 'zip', plugin_path)
 
     return build_zip
 
-path = create_zip('okta')
-blob = bucket.blob('test.zip')
-blob.upload_from_filename(path)
-sourceUrl = f'gs://{blob.bucket.name}/{blob.name}'
+def create_bucket(plugin):
+    path = create_zip(plugin)
+    blob = bucket.blob('test.zip')
+    blob.upload_from_filename(path)
+    object_url = f'gs://{blob.bucket.name}/{blob.name}'
+    return object_url
+
+sourceURL = create_bucket('okta')
+
+# Get Locations
+locations = service.projects().locations().list(name=f'projects/{project}').execute()
+log_entry(len(locations['locations']))
+#print(json.dumps(locations,indent=2))
+log_entry(locations['locations'][0])
+
+function_name = 'function-3'
+name = f"{locations['locations'][0]['name']}/functions/{function_name}"
+log_entry(name)
 
 # Create Function
 body =  {
-        "name": "projects/canvas-network-282101/locations/us-central1/functions/function-3",
+        #"name": "projects/canvas-network-282101/locations/us-central1/functions/function-3",
+        "name" : name,
         "availableMemoryMb": 128,
         "entryPoint": "lambda_handler",
         "description": "Test function",
@@ -79,30 +88,32 @@ body =  {
         "runtime": "python37",
         "ingressSettings": "ALLOW_ALL",
         "maxInstances": 1,
-        "sourceArchiveUrl": "gs://credkinggcp123456/test.zip",
+        "sourceArchiveUrl": sourceURL,
         "httpsTrigger": {},
         "vpcConnector": "",
-        "serviceAccountEmail": "test-608@canvas-network-282101.iam.gserviceaccount.com"
+        "serviceAccountEmail": credentials.service_account_email
         }
-service.projects().locations().functions().create(location=locations['locations'][0]['name'],body=body).execute()
+log_entry(service.projects().locations().functions().create(location=locations['locations'][0]['name'],body=body).execute())
 
 # Get Status of the function and make sure that it is active
 while True:
+    sleep = 5
     status = service.projects().locations().functions().get(name="projects/canvas-network-282101/locations/us-central1/functions/function-3").execute()
     if status['status'] == 'ACTIVE':
         break
     else:
-        time.sleep(5)
+        log_entry(f"Waiting {sleep} seconds for function to become ACTIVE")
+        time.sleep(sleep)
 
 # Call Function
 data = {"username": "test.test2", "password": "Spring2018", "useragent": "test", "args": {"oktadomain": "cardinalb2e.okta.com"}}
 body = {"data": json.dumps(data)}
 #body = {"data": "{\"username\": \"test.test1\", \"password\": \"Spring2018\", \"useragent\": \"test\", \"args\": {\"oktadomain\": \"cardinalb2e.okta.com\"}}"}
-print(service.projects().locations().functions().call(name="projects/canvas-network-282101/locations/us-central1/functions/function-3",body=body).execute())
+log_entry(service.projects().locations().functions().call(name="projects/canvas-network-282101/locations/us-central1/functions/function-3",body=body).execute())
 
 
 # Delete Function
-print(service.projects().locations().functions().delete(name="projects/canvas-network-282101/locations/us-central1/functions/function-3").execute())
+log_entry(service.projects().locations().functions().delete(name="projects/canvas-network-282101/locations/us-central1/functions/function-3").execute())
 
 # Delete Code
 blob = bucket.blob('test.zip')
@@ -114,6 +125,7 @@ bucket.delete()
 filelist = [ f for f in os.listdir('build') if f.endswith(".zip") ]
 for f in filelist:
     os.remove(os.path.join('build', f))
+    log_entry(f"Removing file {f}")
 
 # TODO: Parameterize above
 # TODO: Make into functions with a main method
