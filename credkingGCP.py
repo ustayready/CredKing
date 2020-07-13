19f09c302954a313c49dff3c3146002015a35123
hawkeyeone <ron@Desktop-Ron.localdomain>
2020-07-12 21:09:36 -0400

    Adding new param for environments

--- credkingGCP.py
+++ credkingGCP.py
@@ -26,6 +26,11 @@ def main(args,pargs):
     password_file = args.passwordfile
     sa_file = args.sa_creds_file
     useragent_file = args.useragentfile
+    print(args.env)
+    sys.exit(0)
+
+
+    sys.exit(0)
 
     pluginargs = {}
     for i in range(0, len(pargs) - 1):
@@ -274,14 +279,23 @@ def delete_zip():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    # Required Fields
     parser.add_argument('--plugin', help='spraying plugin', required=True)
     parser.add_argument('--threads', help='thread count (default: 1)',
                         type=int, default=1)
     parser.add_argument('--userfile', help='username file', required=True)
     parser.add_argument('--passwordfile', help='password file', required=True)
+    parser.add_argument('--env', help="Serverless environments", required=True, action='append')
+
+    # Optional Fields
     parser.add_argument('--useragentfile', help='useragent file', required=False)
+
+    # AWS Environment Required Fields
     parser.add_argument('--access_key', help='aws access key', required=False)
     parser.add_argument('--secret_access_key', help='aws secret access key', required=False)
+
+    # GCP Environment Required Fields
     parser.add_argument('--sa_creds_file', help="GCP Json Keys", required=False)
-    args,pluginargs = parser.parse_known_args()
-    main(args,pluginargs)
+
+    args,plugin_args = parser.parse_known_args()
+    main(args,plugin_args)
