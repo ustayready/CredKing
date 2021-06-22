9715e07b112530aa95167d12b26e8ae086ee0e70
Jonathan Echavarria <jrec93@gmail.com>
2021-06-22 17:18:24 -0400

    bugfix and indentation fixes

--- plugins/basic_auth/basic_auth.py
+++ plugins/basic_auth/basic_auth.py
@@ -3,34 +3,35 @@ from requests.auth import HTTPBasicAuth
 
 
 def cred_check(url, user, password):
-	if (password) > 0:
-		result = None
-		result = requests.get(url, auth=HTTPBasicAuth(user, password))
-		if result.status_code == 200:
-			return True
+    headers = {
+        'User-Agent': ''
+    }
+    if (password) is not None:
+        result = None
+        result = requests.get(url, auth=HTTPBasicAuth(user, password), headers=headers)
+        print(result.status_code)
+        if result.status_code == 200:
+            return True
 
 
-def basic_auth(url, user, password):
-	data_response = {
-		'code': 'notset',
-		'success': False,
-		'url': url,
-		'user': user,
-		'password': password
-	}
-	try:
-		auth_check = cred_check(url, user, password)
-		if auth_check:
-			data_response['success'] = True
-
-	except Exception as ex:
-		data_response['error'] = ex
-		pass
-
-	return data_response
+def basic_auth(url,user,password):
+    data_response = {
+        'code': 'notset',
+        'success': False,
+        'url': url,
+        'user': user,
+        'password': password
+    }
+    try:
+        auth_check = cred_check(url, user, password)
+        if auth_check:
+            data_response['success'] = True
+    except Exception as ex:
+        data_response['error'] = ex
+        pass
+    return data_response
 
 
 def lambda_handler(event, context):
-	url = event['args']['url']
-	return basic_auth(url, event['user'], event['password'])
-
+    url = event['args']['url']
+    return basic_auth(url, event['user'], event['password'])
